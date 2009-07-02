918cbe47ad225d72c979ddcd7a51c652b20177f9
Florian Anderiasch <fa@art-core.org>
2009-07-03 01:48:49 +0200

    added 301 and add() now redirects to _id_

--- shortcore.config.php
+++ shortcore.config.php
@@ -13,7 +13,7 @@ $cfg = array(
     'tpl_body' => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
 <head> 
-<title>ACShortener</title> 
+<title>Shortcore</title> 
 <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" /> 
 </head> 
 <body>
--- shortcore.php
+++ shortcore.php
@@ -15,7 +15,7 @@ $cfg = array(
     'tpl_body' => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title>ACShortener</title>
+<title>Shortcore</title>
 <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
 </head>
 <body>
@@ -142,7 +142,7 @@ class Shortcore {
                                 $this->cfg['table'], $id, $url, $title, $time);
             if ($this->DEBUG) print_r($sql_insert);
         $this->db->query($sql_insert);
-        $this->page($url);
+        $this->page($cfg['home'].'_'.$id.'_');
     }
 
     /**
@@ -154,6 +154,7 @@ class Shortcore {
             $arg = $this->cfg['home'];
         }
         $this->_e('[page] '.$arg);
+        header('HTTP/1.1 301 Moved Permanently');
         header('Location: '.$arg);
         exit;
     }
